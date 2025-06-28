985e1c952e300bca15f1e02db7b6870df8e66fa1
user <gres.xedny@yandex.ru>
2025-06-29 01:08:39 +0300

    Create design

---
+++ app/src/main/java/com/bederin/userinfo001/Main.java
@@ -0,0 +1,25 @@
+package com.bederin.userinfo001;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+    TextView tvName, tvYear;;
+    EditText etName, etYear;
+    Button btnGoMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+    }
+}
